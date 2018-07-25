b238400ebbf09137feab41d2e34cfddc4488cf9b
Robert Oleksy <robertoleksy6@gmail.com>
2018-07-25 11:41:09 +0200

    optimalizations

--- src/cAsio_udp.cpp
+++ src/cAsio_udp.cpp
@@ -1,4 +1,5 @@
 #include "cAsio_udp.h"
+#include <iostream>
 
 cAsio_udp::cAsio_udp(boost::asio::ip::udp::socket && sock)
 :   m_sock(std::move(sock))
@@ -7,7 +8,7 @@ cAsio_udp::cAsio_udp(boost::asio::ip::udp::socket && sock)
 
 size_t cAsio_udp::send(const unsigned char * data, size_t data_size, const boost::asio::ip::address & adr) {
     boost::asio::ip::udp::endpoint receiver_endpoint(adr, m_port);
-    if (!m_sock.is_open()) {
+/*    if (!m_sock.is_open()) {
         if( adr.is_v4() ) {
             m_sock.open(boost::asio::ip::udp::v4());
         } else if( adr.is_v6() ) {
@@ -15,8 +16,7 @@ size_t cAsio_udp::send(const unsigned char * data, size_t data_size, const boost
         } else {
             return 0;
         }
-    }
-
+    }*/
     return m_sock.send_to(boost::asio::buffer(data, data_size), receiver_endpoint);
 }
 
--- src/node.cpp
+++ src/node.cpp
@@ -9,18 +9,21 @@ void node::run() {
     std::array<unsigned char, 9000> buffer;
     std::array<unsigned char, crypto_stream_KEYBYTES> crypto_key;
     crypto_key.fill(0b10101010);
-    boost::asio::ip::address dst_addr = boost::asio::ip::address::from_string("192.168.1.106");
+    boost::asio::ip::address dst_addr = boost::asio::ip::address::from_string("192.168.1.66");
     assert(m_tun != nullptr);
     assert(m_io_service != nullptr);
     m_tun->set_ip(boost::asio::ip::address::from_string("fd44:1111:2222:3333:4444:5555:6666:7777"), 1500);
 	while (true) {
         size_t tun_read_size = m_tun->read_from_tun(buffer.data(), buffer.size());
+//        std::cout << "readed from tun " << tun_read_size << "B\n";
         size_t encypted_message_size =
                 m_crypto->encrypt(
                           buffer.data(), tun_read_size,
                           crypto_key.data(), crypto_key.size(),
                           buffer.data(), buffer.size());
-        m_udp->send(buffer.data(), encypted_message_size, dst_addr);
+//        std::cout << "encrypted\n";
+        size_t udp_sended = m_udp->send(buffer.data(), encypted_message_size, dst_addr);
+//        std::cout << "sended via udp " <<  udp_sended << "\n";
     }
 }
 
@@ -33,6 +36,7 @@ node node::node_factory() {
     node_product.m_tun = std::make_unique<linuxTun<>>(std::move(stream_descriptor));
     assert(stream_descriptor == nullptr);
     boost::asio::ip::udp::socket socket(*(node_product.m_io_service));
+    socket.open(boost::asio::ip::udp::v4());
     node_product.m_udp = std::make_unique<cAsio_udp>(std::move(socket));
     return node_product;
 }
