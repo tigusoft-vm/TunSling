5666e03501cea5aea89aa54b3fcf989ef6162ca6
Robert Oleksy <robertoleksy6@gmail.com>
2018-08-21 14:00:43 +0200

    fix warnings

--- src/ccryptoempty.cpp
+++ src/ccryptoempty.cpp
@@ -1,12 +1,12 @@
 #include "ccryptoempty.h"
 #include <stdexcept>
 
-size_t cCryptoEmpty::encrypt(const unsigned char *input_data, size_t input_data_size, const unsigned char *key, size_t key_size, unsigned char *output_data, size_t output_data_max_size) {
+size_t cCryptoEmpty::encrypt(const unsigned char *input_data, size_t input_data_size, const unsigned char *, size_t, unsigned char *output_data, size_t) {
     if (input_data != output_data) throw std::invalid_argument("input_data != output_data");
     return input_data_size;
 }
 
-size_t cCryptoEmpty::decrypt(const unsigned char *input_data, size_t input_data_size, const unsigned char *key, size_t key_size, unsigned char *output_data, size_t output_data_max_size) {
+size_t cCryptoEmpty::decrypt(const unsigned char *input_data, size_t input_data_size, const unsigned char *, size_t, unsigned char *output_data, size_t) {
     if (input_data != output_data) throw std::invalid_argument("input_data != output_data");
     return input_data_size;
 }
