63c364de32c903405c71b24eacb6fa1f7fb9a0c8
Robert Oleksy <robertoleksy6@gmail.com>
2018-08-28 13:45:18 +0200

    [wip][bad] async tun

--- src/node.cpp
+++ src/node.cpp
@@ -38,5 +38,41 @@ void node::run() {
             size_t udp_sended = m_udp->send(buffer.data(), encypted_message_size, m_dst_addr);
             buffer.release();
 		}
+    }
+}
+
+void node::run_async_tun(size_t number_of_tun_threads) {
+	std::array<unsigned char, crypto_secretbox_KEYBYTES> crypto_key;
+	crypto_key.fill(0b10101010);
+	cBufferManager bufferManager(number_of_tun_threads, 1500 * 20); // for possible weld data, 1 buffer for 1 thread
+	std::vector<std::thread> thread_vector;
+	
+	for (size_t i = 0; i < number_of_tun_threads; i++) {
+		cBuffer & buffer = bufferManager.get_free_buffer_or_wait();
+		m_tun_async->async_read_from_tun(buffer.data(), buffer.size(),
+			[&buffer, this, &crypto_key](size_t tun_read_size) {
+			size_t encypted_message_size =
+				m_crypto->encrypt(
+					  buffer.data(), tun_read_size,
+					  crypto_key.data(), crypto_key.size(),
+					  buffer.data(), buffer.size());
+				std::lock_guard<std::mutex> lg(m_udp_mutex);
+				size_t udp_sended = m_udp->send(buffer.data(), encypted_message_size, m_dst_addr);
+			}
+		);
+		
 	}
+	
+	for (size_t i = 0; i < number_of_tun_threads; i++) {
+		thread_vector.emplace_back([this]{
+			m_tun_async->run();
+		});
+	}
+	for (auto & thread : thread_vector)
+		thread.join();
+}
+
+void node::async_read_from_tun_handler(size_t tun_read_size)
+{
+	
 }
--- src/node.h
+++ src/node.h
@@ -12,18 +12,22 @@
 class node final {
     public:
         node() = default;
-        void run();
+        void run(); ///< sync tun
+        void run_async_tun(size_t number_of_tun_threads); ///< multiple tun threads
         node(node &&) = default;
     private:
         boost::asio::ip::address m_dst_addr;
         std::unique_ptr<boost::asio::io_service> m_io_service;
         std::unique_ptr<iCrypto> m_crypto;
         std::unique_ptr<iTun> m_tun;
+        std::unique_ptr<iTunAsync> m_tun_async;
         std::unique_ptr<iUdp> m_udp;
         std::mutex m_udp_mutex;
         std::unique_ptr<ThreadPool> m_thread_pool;
 
         friend class cNode_factory;
+		
+		void async_read_from_tun_handler(size_t tun_read_size);
 };
 
 #endif // NODE_H
