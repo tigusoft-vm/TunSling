2ee2290b42f8534fbbf3227ff8c2ba39d82d57cd
sdf5 <sdf5@meshnet.pl>
2018-08-29 12:14:03 +0200

    Add conflict between threads and tunMultiThread options

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 		desc.add_options()
 			("help", "Produce help message")
 			("address", po::value<std::string>()->required(), "Address (required)")
-			("threads", po::value<int>()->required(), "Number threads for crypto and send UDP, if 0 then disable threadpool (required)")
+			("threads", po::value<int>()->default_value(0), "Number threads for crypto and send UDP, if 0 then disable threadpool")
 			("UDP", po::value<std::string>()->required(), "UDP type (Asio, Sendmmsg, Empty) (required)")
 			("crypto", po::value<std::string>()->required(), "Crypto type (Secretbox, X_salsa_20, Empty) (required)")
 			("tun", po::value<std::string>()->required(), "Tun type (LinuxNormal, LinuxWeld) (required)")
@@ -31,6 +31,11 @@ int main(int argc, char *argv[])
 		}
 		
 		po::notify(vm);
+
+		if (vm.count("threads") && !vm["threads"].defaulted() 
+			&& vm.count("tunMultiThread"))
+			throw logic_error(string("Conflicting options '") 
+							  + "threads" + "' and '" + "tunMultiThread" + "'.");
 		
 		cNode_factory factory;
 		auto my_node = factory.create_node( vm );
